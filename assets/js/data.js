ffbd3c4aa841b1e40a62434d2f6356c6db8950d9
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-06-05 13:56:54 +0700

    main

---
+++ assets/js/data.js
@@ -0,0 +1,31 @@
+const posts = {
+    1 : {
+        image : "esport.jpg",
+        title : "Apa itu E-sport",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia"
+    },
+    2 : {
+        image : "history.jpg",
+        title : "Sejarah",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    },
+    3 : {
+        image : "cabang.jpg",
+        title : "Cabang E-sport",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    }
+
+}
+
+const cabang = {
+    1 : {
+        image : "mlbb.jpg",
+        title : "Mobile Legends Bang Bang",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    },
+    2 : {
+        image : "mlbb.jpg",
+        title : "Mobile Legends Bang Bang",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 var slideIndex = 1;
 
+let posts = {
+    a : {
+        image : "esport.jpg",
+        title : "Apa itu E-sport",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia"
+    },
+    b : {
+        image : "history.jpg",
+        title : "Sejarah",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    },
+    c : {
+        image : "cabang.jpg",
+        title : "Cabang E-sport",
+        data : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quia tenetur, cumque impedit facere ipsam, harum libero possimus iusto error eligendi quos sunt, hic nihil architecto porro soluta expedita. Mollitia" 
+    }
+
+}
+
 slideShow(slideIndex);
+postShow();
 
 function plusSlide(n){
     slideShow(slideIndex += n);
@@ -19,4 +39,27 @@ function slideShow(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex-1].style.display = "block";
+}
+
+function* entries(obj) {
+    for (let key of Object.keys(obj)) {
+      yield [key, obj[key]];
+    }
+ }
+ 
+
+function postShow() {
+    let item_post = document.querySelector("#content");
+
+    for (const [key, value] of entries(posts)){
+        // console.log(value.title);
+        let data = document.createElement('article');
+        data.classList.add('card');
+        data.innerHTML = "<h2>"+value.image+"</h2>";
+        data.innerHTML += "<h2>"+value.title+"</h2>";
+        data.innerHTML += "<p>"+value.data+"</p>";
+
+
+        item_post.appendChild(data);
+    }
 }
