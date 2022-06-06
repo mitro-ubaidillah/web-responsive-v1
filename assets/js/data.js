0ee8d967e3e1d9fea0aae6b6e9e96f8c3e787a8a
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-06-06 20:16:53 +0700

    finish

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,37 +1,39 @@
 const posts = {
     1 : {
+        id : "esport",
         image : "esport.jpg",
         title : "Apa itu E-sport",
         data1 : "merupakan suatu istilah untuk kompetisi permainan video pemain jamak, umumnya antara para pemain profesional.Aliran permainan video yang biasanya dihubungkan dengan olahraga elektronik adalah aliran strategi waktu-nyata, perkelahian, tembak-menembak orang-pertama, dan arena pertarungan daring multipemain. Turnamen seperti The International Dota 2 Championships, League of Legends World Championship, Battle.net World Championship Series, Evolution Championship Series, Intel Extreme Masters, menampilkan siaran langsung serta hadiah tunai pada para pemainnya",
         data2 : "Meski kompetisi teroganisasi telah lama menjadi bagian dari budaya permainan video, kompetisi ini telah mengalami peningkatan besar dalam popularitas dari akhir dekade 2000-an dan awal 2010-an. Jika kompetisi di dekade 2000-an kebanyakan diikuti oleh para pemain amatir, pengadaan kompetisi profesional dan meningkatnya pemirsa saat ini mendukung munculnya banyak pemain dan tim profesional secara signifikan, dan banyak pengembang permainan video saat ini membangun permainan dengan corak untuk memfasilitasi kompetisi tersebut."
     },
     2 : {
+        id : "history",
         image : "history.jpg",
         title : "Sejarah",
         data1 : "Turnamen olahraga elektronik pertama terjadi pada 19 Oktober 1972 di Universitas Standford. Turnamen tersebut berjudul Spacewar! dengan bertajuk turnamen Intergalactic Spacewar Olympic. Turnamen tersebut dimenangkan oleh Stewart Brand, seorang mahasiswa jurusan Biologi. Ia mendapatkan hadiah berupa jasa berlangganan majalah Rolling Stone selama satu tahun gratis. Di tahun 1980, diadakan turnamen olahraga elektronik terbesar pertama dengan jumlah partisipan mencapai 10.000 pemain. Sejak saat itu, olahraga elektronik menjadi perbicangan hingga ditayangkan dalam acara televisi.",
         data2 : "Di Indonesia, sebelum menjadi olahraga elektronik bermula dari permainan elektronik terlebih dahulu yang dimainkan hanya karena kegemaran saja. Di tahun 1999, permainan daring pertama kali dipertandingkan, dengan judul permainan Starcraft dan Quake II. Di tahun 2002, diadakan turnamen yang bernama Cyber Game, pada saat itu game yang dimainkan bertambah antara lain Age of Empires II, Counter Strike, dan FIFA World Cup."
     },
     3 : {
+        id : "jenis",
         image : "jenis.jpg",
         title : "Jenis",
         data : "Pada Esport terdapat banyak jenis permain yang dibagi dari cara bermainnya. ada olahraga, FPS, battle royal, moba dan masih banyak lagi. berikut ini penjelasan mengenai beberapa jenis lomba pada Esport :"
     }
 
 }
-
 const jenis = {
     1 : {
-        image : "mlbb.jpg",
+        image : "moba.jpg",
         title : "Multiplayer Online Battle Arena (MOBA) atau arena pertarungan daring multipemain",
-        data : "MOBA merupakan jenis olahraga elektronik yang bertujuan untuk menghancurkan objek utama milik lawan untuk memenangkan pertandingan. Pertandingan dilakukan secara tim, di mana satu tim biasanya terdiri dari lima orang.[11] Para anggota dari tim tersebut memiliki tugas masing-masing, seperti penyerang (assassin), menyerang jarak dekat (fighter), penyerang jarak jauh (marksman), dan ada yang fokus terhadap pertahanan. Contohnya :"
+        data : "MOBA merupakan jenis olahraga elektronik yang bertujuan untuk menghancurkan objek utama milik lawan untuk memenangkan pertandingan. Pertandingan dilakukan secara tim, di mana satu tim biasanya terdiri dari lima orang.[11] Para anggota dari tim tersebut memiliki tugas masing-masing, seperti penyerang (assassin), menyerang jarak dekat (fighter), penyerang jarak jauh (marksman), dan ada yang fokus terhadap pertahanan."
     },
     2 : {
-        image : "mlbb.jpg",
+        image : "fps.jpg",
         title : "First Person Shooter (FPS) atau tembak menembak sudut pandang orang pertama",
         data : "First Person Shooter (FPS) merupakan salah satu jenis permainan yang menggunakan sudut pandang orang pertama, yang menjadikan pemain pertama sebagai karakter utama dalam permainan yang membawa alat tempur, seperti senjata api, pisau, granat, dan sebagainya." 
     },
     3 : {
-        image : ".jpg",
+        image : "pes.jpg",
         title : "Balapan dan Olahraga",
         data : "Permainan jenis ini sama halnya dengan cara konvensional seperti balap motor, sepakbola, atau basket yang dirubah konsepnya menjadi kompetisi elektronik. Genre ini juga mulai berkembang bahkan punya sistem layaknya olahraga di dunia nyata. Para pemain direkrut untuk mewakili organisasi olahraga yang sudah ada."
     }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 var slideIndex = 1;
-
 slideShow(slideIndex);
 postShow();
-
+function myFunction() {
+    var nav = document.getElementById("myTopNav");
+    if (nav.className === "top-nav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "top-nav";
+    }
+}
 function plusSlide(n){
     slideShow(slideIndex += n);
 }
-
 function slideShow(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
@@ -21,38 +26,38 @@ function slideShow(n) {
     }
     slides[slideIndex-1].style.display = "block";
 }
-
 function* entries(obj) {
     for (let key of Object.keys(obj)) {
       yield [key, obj[key]];
     }
  }
- 
-
 function postShow() {
     let item_post = document.querySelector("#content");
-
     for (const [key, value] of entries(posts)){
-        // console.log(value.title);
         let data = document.createElement('article');
         data.classList.add('card');
         if(value.data1 != undefined){
-            data.innerHTML = "<h2>"+value.image+"</h2>";
+            data.id = value.id;
+            data.innerHTML = "<img src='assets/image/"+value.image+"' class='futured-image' />";
             data.innerHTML += "<h2>"+value.title+"</h2>";
             data.innerHTML += "<p>"+value.data1+"</p>";
             data.innerHTML += "<p>"+value.data2+"</p>";
-            if(value.title == "Jenis"){
-                for(const [key, value] of entries(jenis))
-                data.innerHTML += "<h3>"+value.title+"</h3>";
-            }
-        }else{
-            data.innerHTML = "<h2>"+value.image+"</h2>";
-            data.innerHTML += "<h2>"+value.title+"</h2>";
-            data.innerHTML += "<p>"+value.data+"</p>";
-            if(value.title == "Jenis"){
+            if(value.title === "Jenis"){
                 for(const [key, value] of entries(jenis)){
                 data.innerHTML += "<h3>"+value.title+"</h3>";
-                data.innerHTML += "<h3>"+value.image+"</h3>";
+                data.innerHTML += "<img src='assets/image/"+value.image+"' class='sub-image' />";
+                data.innerHTML += "<p>"+value.data+"</p>";
+                }
+            }
+        }else{
+            data.id = value.id;
+            data.innerHTML = "<img src='assets/image/"+value.image+"' class='futured-image' />";
+            data.innerHTML += "<h2>"+value.title+"</h2>";
+            data.innerHTML += "<p>"+value.data+"</p>";
+            if(value.title === "Jenis"){
+                for(const [key, value] of entries(jenis)){
+                data.innerHTML += "<h3>"+value.title+"</h3>";
+                data.innerHTML += "<img src='assets/image/"+value.image+"' class='sub-image' />";
                 data.innerHTML += "<p>"+value.data+"</p>";
                 }
             }
